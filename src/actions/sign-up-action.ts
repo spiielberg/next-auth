4ef43a37bf743b520bf86c8066735044affc76e0
Spielberg Hanielly <spiielberg@gmail.com>
2024-05-06 19:32:55 -0300

    feat: Add prisma, create prisma schema and implement sign up with password encryption

--- src/actions/sign-up-action.ts
+++ src/actions/sign-up-action.ts
@@ -1,16 +1,35 @@
 'use server'
 
+import { getUserByEmail } from '@/data/user'
+import { db } from '@/lib/db'
 import { SignUpSchema } from '@/schemas/sign-up-schema'
+import bcrypt from 'bcrypt'
 import { z } from 'zod'
 
 export const signUp = async (value: z.infer<typeof SignUpSchema>) => {
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
   const validatedFields = SignUpSchema.safeParse(value)
 
   if (!validatedFields.success) {
     return { error: 'Invalid fields.' }
   }
+
+  const { name, email, password } = validatedFields.data
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: 'Email already in use.' }
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
 
   return { success: 'Email sent.' }
 }
