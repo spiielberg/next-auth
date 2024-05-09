0c7c84fc840aefc5299fd38b3c0b99b6bd559cea
Spielberg Hanielly <spiielberg@gmail.com>
2024-05-09 11:34:37 -0300

    refactor: Update user model to use emailVerifiedAt instead of emailVerified

--- src/auth.ts
+++ src/auth.ts
@@ -43,7 +43,7 @@ export const {
     linkAccount: async ({ user }) => {
       await db.user.update({
         where: { id: user.id },
-        data: { emailVerified: new Date() },
+        data: { emailVerifiedAt: new Date() },
       })
     },
   },
